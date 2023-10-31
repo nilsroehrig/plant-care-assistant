a7f355d970762fa7dcc7f1df49ad0765fb8d3300
Nils Röhrig <1717688+nilsroehrig@users.noreply.github.com>
2023-10-31 18:22:07 +0100

    add login form

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,56 @@
+<script>
+	import '@picocss/pico';
+	// Import the functions you need from the SDKs you need
+	import { initializeApp } from 'firebase/app';
+	import { setContext } from 'svelte';
+	// TODO: Add SDKs for Firebase products that you want to use
+	// https://firebase.google.com/docs/web/setup#available-libraries
+
+	// Your web app's Firebase configuration
+	const firebaseConfig = {
+		apiKey: 'AIzaSyCXM-aOfj75tbwTYU9IC4SLlVnLsjzo4T0',
+		authDomain: 'fir-playground-22c29.firebaseapp.com',
+		projectId: 'fir-playground-22c29',
+		storageBucket: 'fir-playground-22c29.appspot.com',
+		messagingSenderId: '749449297551',
+		appId: '1:749449297551:web:cb398016eb7c9f57a2bafa'
+	};
+
+	// Initialize Firebase
+	const app = initializeApp(firebaseConfig);
+    setContext("app", app);
+</script>
+
+<div class="wrapper">
+	<header class="container">
+		<nav>
+			<ul>
+				<li><strong>Plant Care Assistant</strong></li>
+			</ul>
+		</nav>
+	</header>
+
+	<main class="container">
+		<slot />
+	</main>
+
+	<footer class="container">SvelteKit &hearts; Firebase</footer>
+</div>
+
+<style>
+	.wrapper {
+		display: grid;
+		min-height: 100vh;
+		grid-template-rows: min-content 1fr min-content;
+	}
+
+	main {
+		align-self: center;
+	}
+
+	footer {
+		padding: 1rem 0;
+		font-size: smaller;
+		text-align: center;
+	}
+</style>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,101 @@
+<article class="grid">
+	<div>
+		<hgroup>
+			<h1>Sign in</h1>
+			<h2>Welcome to Plant Care Assistant</h2>
+		</hgroup>
+		<form>
+			<input
+				type="text"
+				name="login"
+				placeholder="Login"
+				aria-label="Login"
+				autocomplete="nickname"
+				required
+			/>
+			<input
+				type="password"
+				name="password"
+				placeholder="Password"
+				aria-label="Password"
+				autocomplete="current-password"
+				required
+			/>
+			<fieldset>
+				<label for="remember">
+					<input type="checkbox" role="switch" id="remember" name="remember" />
+					Remember me
+				</label>
+			</fieldset>
+			<button type="submit" onclick="event.preventDefault()">Login</button>
+		</form>
+	</div>
+	<div class="image" />
+</article>
+<p>
+	Photo by <a
+		href="https://unsplash.com/de/@feeypflanzen?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+		>feey</a
+	>
+	on
+	<a
+		href="https://unsplash.com/de/fotos/grune-pflanze-im-weissen-topf-qoegZJ3ybOY?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+		>Unsplash</a
+	>
+</p>
+
+<style>
+	article {
+		padding: 0;
+		overflow: hidden;
+	}
+
+	article div {
+		padding: 1rem;
+	}
+
+	p {
+		text-align: right;
+		font-size: smaller;
+		padding: 0.5rem;
+	}
+
+	@media (min-width: 576px) {
+		article div {
+			padding: 1.25rem;
+		}
+	}
+
+	@media (min-width: 768px) {
+		article div {
+			padding: 1.5rem;
+		}
+	}
+
+	@media (min-width: 992px) {
+		article div {
+			padding: 1.75rem;
+		}
+	}
+
+	@media (min-width: 1200px) {
+		article div {
+			padding: 2rem;
+		}
+	}
+
+	/* Hero Image */
+	article div:nth-of-type(2) {
+		display: none;
+		background-color: #374956;
+		background-image: url('/feey-qoegZJ3ybOY-unsplash.jpg');
+		background-position: center 60%;
+		background-size: cover;
+	}
+
+	@media (min-width: 992px) {
+		.grid > div:nth-of-type(2) {
+			display: block;
+		}
+	}
+</style>
