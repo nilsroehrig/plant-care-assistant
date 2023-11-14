a03edbdcdbd09b09246d2c41853d7d44dd898947
Nils Röhrig <1717688+nilsroehrig@users.noreply.github.com>
2023-11-14 13:53:13 +0100

    remove comment

--- src/lib/domain/Plant.ts
+++ src/lib/domain/Plant.ts
@@ -1,27 +1,5 @@
-/*
-
-name: formParseResult.data.name,
-wateringIntervalInHours: formParseResult.data.watering_interval,
-amountPerWateringInMilliliters: formParseResult.data.watering_amount,
-lastWatered: getLastWateredValue(formParseResult.data.last_watering),
-furtherInstructions: formParseResult.data.instructions,
-owner: uid,
-created: FieldValue.serverTimestamp()
-
-*/
-
 import { Timestamp } from 'firebase-admin/firestore';
 import { z } from 'zod';
-
-// const Plant = z.object({
-// 	name: z.string(),
-// 	wateringIntervalInHours: z.number(),
-// 	amountPerWateringInMilliliters: z.number(),
-// 	lastWatered: z.instanceof(Timestamp),
-// 	furtherInstructions: z.string().nullish(),
-// 	owner: z.string(),
-// 	created: z.instanceof(Timestamp)
-// });
 
 export const PlantDto = z.object({
 	id: z.string(),
